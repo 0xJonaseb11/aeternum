cab37ef882d833fca11f5d8f42e42058146cb523
Jonas Sebera <123830791+0xJonaseb11@users.noreply.github.com>
2026-02-25 02:06:25 -0500

    [update]: corrected paths (#13)
    * [docs]
    
    * [chore]
    
    * [update]: EvidenceVault contract
    
    * [update]: Evidence vault storage contract
    
    * [update]: groth16 verifier
    
    * [update]: Evidence vault interface
    
    * [update]: zkVerifier + mocks
    
    * [update]: zk setup script
    
    * [update]: zk upgrade script
    
    * [update]: utils + docs
    
    * [update]: evidence vault tests
    
    * [docs]
    
    * [update]: corrected paths
    
    ---------
    
    Co-authored-by: 0xJonaseb11 <0xJonaseb11@users.noreply.github.com>

--- packages/hardhat/contracts/EvidenceVault.sol
+++ packages/hardhat/contracts/EvidenceVault.sol
@@ -13,8 +13,8 @@ import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/P
 // ─────────────────────────────────────────────────────────────────────────────
 //  Local
 // ─────────────────────────────────────────────────────────────────────────────
-import { IEvidenceVault } from "../../interface/IEvidenceVault.sol";
-import { IZKVerifier } from "../interface/IZKVerifier.sol";
+import { IEvidenceVault } from "./interface/IEvidenceVault.sol";
+import { IZKVerifier } from "./interface/IZKVerifier.sol";
 import { EvidenceVaultStorage } from "./EvidenceVaultStorage.sol";
 
 /**
