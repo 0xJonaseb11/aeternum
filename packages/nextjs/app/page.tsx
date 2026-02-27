879acfed35882d06b5b12f2c50e76510181b00fa
0xJonaseb11 <0xJonaseb11@users.noreply.github.com>
2026-02-27 10:05:45 +0200

    [chore+fix]: speed insights + build errors

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -12,10 +12,10 @@ import {
   ShieldCheckIcon,
   Squares2X2Icon,
 } from "@heroicons/react/24/outline";
-import deployedContracts from "~~/contracts/deployedContracts";
 import { ProofListSkeleton } from "~~/components/ui/Skeleton";
 import { EvidenceList } from "~~/components/vault/EvidenceList";
 import { UploadEvidence } from "~~/components/vault/UploadEvidence";
+import deployedContracts from "~~/contracts/deployedContracts";
 import { useTargetNetwork } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
